402973883c3f7a4ed16165d1a839fea3dd50e187
wasifraza909 <wasifraza909@gmail.com>
2021-07-31 13:21:24 +0500

    Completed Navbar with styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Wasssup</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Route path='/' />
+    </Router>
   );
 }
 
