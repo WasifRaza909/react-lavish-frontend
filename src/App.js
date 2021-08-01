9f0ce47a17c971f05de4772f872d2d868ad2f0e4
wasifraza909 <wasifraza909@gmail.com>
2021-08-01 14:36:14 +0500

    Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/pages/HomePage/Home';
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
+import Footer from './components/pages/Footer/Footer';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Route path='/' exact component={Home} />
+      <Route path='/services'  component={Services} />
+      <Route path='/products'  component={Products} />
+      <Route path='/sign-up'  component={SignUp} />
+      
+      <Footer />
     </Router>
   );
 }
